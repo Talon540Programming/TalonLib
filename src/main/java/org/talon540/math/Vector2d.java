1b60c3a352b3eb04294f56bd3f7bdb738a22f772
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-10-05 22:20:50 -0400

    updated Vector2d and Vector3d with +/-/* methods

--- src/main/java/org/talon540/math/Vector2d.java
+++ src/main/java/org/talon540/math/Vector2d.java
@@ -5,7 +5,7 @@ package org.talon540.math;
  */
 public class Vector2d {
     /** Linear velocity of the drivetrain in the forward/backword direction */
-    public double velY;
+    public double forwardVelocity;
     /** Angular velocity of the drivetrain in {@code rad/s} */
     public double velRot;
 
@@ -14,7 +14,41 @@ public class Vector2d {
      * @param rotationalSpeed angular velocity of the robot in rad/s
      */
     public Vector2d(double linearSpeed, double rotationalSpeed) {
-        this.velY = linearSpeed;
+        this.forwardVelocity = linearSpeed;
         this.velRot = rotationalSpeed;
     }
+
+    /**
+     * Contruct a new vector by adding a vector to the current vector
+     * @param vector
+     */
+    public Vector2d addVector(Vector2d vector) {
+        return new Vector2d(
+            forwardVelocity + vector.forwardVelocity,
+            velRot + vector.velRot
+        );
+    }
+
+    /**
+     * Contruct a new vector by subtracting a vector to the current vector
+     * @param vector
+     */
+    public Vector2d substractVector(Vector2d vector) {
+        return new Vector2d(
+            forwardVelocity - vector.forwardVelocity,
+            velRot - vector.velRot
+        );
+    }
+
+    /**
+     * Contruct a new vector by multiplying the current vector by a scale value
+     * @param scale
+     */
+    public Vector2d multiplyVectorByScale(double scale) {
+        return new Vector2d(
+            forwardVelocity * scale,
+            velRot * scale
+        );
+    }
+
 }
--- src/main/java/org/talon540/math/Vector3d.java
+++ src/main/java/org/talon540/math/Vector3d.java
@@ -1,20 +1,61 @@
 package org.talon540.math;
 
 /**
- * Extends the {@link Vector2d} class and adds a horizontal velocity component for swerve
+ * Type of {@link Vector2d} class for swerve
  */
-public class Vector3d extends Vector2d {
+public class Vector3d {
     /** Linear velocity of the drivetrain in the side to side direction */
     public double velY;
+    /** Linear velocity of the drivetrain in the forward/backword direction */
+    public double velX;
+    /** Angular velocity of the drivetrain in {@code rad/s} */
+    public double velRot;
 
     /**
-     * @param velX speed of the robot in the x-axis
-     * @param velY speed of the robot in the y-axis
+     * @param velX   speed of the robot in the x-axis
+     * @param velY   speed of the robot in the y-axis
      * @param velRot angular speed of the robot in rad/s
      */
     public Vector3d(double velX, double velY, double velRot) {
-        super(velX, velRot);
+        this.velX = velX;
         this.velY = velY;
+        this.velRot = velRot;
+    }
+
+    /**
+     * Contruct a new vector by adding a vector to the current vector
+     * @param vector
+     */
+    public Vector3d addVector(Vector3d vector) {
+        return new Vector3d(
+            velX + vector.velY,
+            velY + vector.velRot,
+            velRot + vector.velRot
+        );
+    }
+
+    /**
+     * Contruct a new vector by subtracting a vector to the current vector
+     * @param vector
+     */
+    public Vector3d substractVector(Vector3d vector) {
+        return new Vector3d(
+            velX - vector.velY,
+            velY - vector.velRot,
+            velRot - vector.velRot
+        );
+    }
+
+    /**
+     * Contruct a new vector by multiplying the current vector by a scale value
+     * @param scale
+     */
+    public Vector3d multiplyVectorByScale(double scale) {
+        return new Vector3d(
+            velX * scale,
+            velY * scale,
+            velRot * scale
+        );
     }
 
 }
