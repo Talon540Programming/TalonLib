a03ed12b79db19bfd1dd533334ca1ffd5699f417
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-10-06 23:19:10 -0400

    formatting

--- src/main/java/org/talon540/mapping/BoundRobotPositionTreeMap.java
+++ src/main/java/org/talon540/mapping/BoundRobotPositionTreeMap.java
@@ -2,15 +2,17 @@ package org.talon540.mapping;
 
 import edu.wpi.first.math.geometry.Pose2d;
 
-
 /**
- * Create a type of {@link UnboundRobotPositionMap} which has a specific limit on the number of entries that can be made into the map. This is usually for memory and speed purposes
+ * Create a type of {@link UnboundRobotPositionMap} which has a specific limit
+ * on the number of entries that can be made into the map. This is usually for
+ * memory and speed purposes
  */
 public class BoundRobotPositionTreeMap extends UnboundRobotPositionMap {
     private int mapSize;
 
     /**
      * Create a position map object with a max number of entries
+     * 
      * @param mapSize max number of entries in the map
      */
     public BoundRobotPositionTreeMap(int mapSize) {
@@ -20,7 +22,8 @@ public class BoundRobotPositionTreeMap extends UnboundRobotPositionMap {
     @Override
     public void addPositionToMap(Pose2d position, double timestamp) {
         super.map.put(timestamp, position);
-        if (super.map.size() > this.mapSize) super.map.remove(super.map.firstKey());
+        if (super.map.size() > this.mapSize)
+            super.map.remove(super.map.firstKey());
     }
 
 }
