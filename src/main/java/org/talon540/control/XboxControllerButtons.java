709fb2d3a34a2af884debbbc8d9ad421cdd0b6ba
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-10-09 23:14:52 -0400

    Update XboxControllerButtons.java

--- src/main/java/org/talon540/control/XboxControllerButtons.java
+++ src/main/java/org/talon540/control/XboxControllerButtons.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj2.command.button.Button;
 public class XboxControllerButtons {
     public JoystickButton LEFT_BUMPER, RIGHT_BUMPER, LEFT_STICK, RIGHT_STICK, A, B, X, Y, BACK, START;
     public Button LEFT_TRIGGER, RIGHT_TRIGGER;
-    public Button DAPD_NORTH, DAPD_NORTHEAST, DAPD_EAST, DAPD_SOUTHEAST, DAPD_SOUTH, DAPD_SOUTHWEST, DAPD_WEST, DAPD_NORTHWEST;
+    public Button DPAD_NORTH, DPAD_NORTHEAST, DPAD_EAST, DPAD_SOUTHEAST, DPAD_SOUTH, DPAD_SOUTHWEST, DPAD_WEST, DPAD_NORTHWEST;
 
     public XboxControllerButtons(XboxController controller) {
         LEFT_BUMPER = new JoystickButton(controller, XboxController.Button.kLeftBumper.value);
@@ -26,13 +26,13 @@ public class XboxControllerButtons {
         LEFT_TRIGGER = new XboxControllerTriggerButton(controller, XboxControllerBinds.Triggers.LEFT_TRIGGER);
         RIGHT_TRIGGER = new XboxControllerTriggerButton(controller, XboxControllerBinds.Triggers.RIGHT_TRIGGER);
 
-        DAPD_NORTH = new XboxControllerDPADButton(controller, DPAD.NORTH);
-        DAPD_NORTHEAST = new XboxControllerDPADButton(controller, DPAD.NORTHEAST);
-        DAPD_EAST = new XboxControllerDPADButton(controller, DPAD.EAST);
-        DAPD_SOUTHEAST = new XboxControllerDPADButton(controller, DPAD.SOUTHEAST);
-        DAPD_SOUTH = new XboxControllerDPADButton(controller, DPAD.SOUTH);
-        DAPD_SOUTHWEST = new XboxControllerDPADButton(controller, DPAD.SOUTHWEST);
-        DAPD_WEST = new XboxControllerDPADButton(controller, DPAD.WEST);
-        DAPD_NORTHWEST = new XboxControllerDPADButton(controller, DPAD.NORTHWEST);
+        DPAD_NORTH = new XboxControllerDPADButton(controller, DPAD.NORTH);
+        DPAD_NORTHEAST = new XboxControllerDPADButton(controller, DPAD.NORTHEAST);
+        DPAD_EAST = new XboxControllerDPADButton(controller, DPAD.EAST);
+        DPAD_SOUTHEAST = new XboxControllerDPADButton(controller, DPAD.SOUTHEAST);
+        DPAD_SOUTH = new XboxControllerDPADButton(controller, DPAD.SOUTH);
+        DPAD_SOUTHWEST = new XboxControllerDPADButton(controller, DPAD.SOUTHWEST);
+        DPAD_WEST = new XboxControllerDPADButton(controller, DPAD.WEST);
+        DPAD_NORTHWEST = new XboxControllerDPADButton(controller, DPAD.NORTHWEST);
     }
 }
