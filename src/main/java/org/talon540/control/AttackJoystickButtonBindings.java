dd42182cf2e64477daf680fc30a67c5b8c0d0033
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-10-09 23:09:29 -0400

    added top_bottom joystick button

--- src/main/java/org/talon540/control/AttackJoystickButtonBindings.java
+++ src/main/java/org/talon540/control/AttackJoystickButtonBindings.java
@@ -3,6 +3,7 @@ package org.talon540.control;
 public interface AttackJoystickButtonBindings {
     public int TRIGGER = 0; // TODO
     public int TOP_MIDDLE = 0; // TODO
+    public int TOP_BOTTOM = 0; // TODO
     public int TOP_LEFT = 0; // TODO
     public int TOP_RIGHT = 0; // TODO
     public int LEFT_TOP = 0; // TODO
--- src/main/java/org/talon540/control/AttackJoystickButtons.java
+++ src/main/java/org/talon540/control/AttackJoystickButtons.java
@@ -4,13 +4,14 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 public class AttackJoystickButtons {
-    public JoystickButton TRIGGER, TOP_MIDDLE, TOP_LEFT, TOP_RIGHT, LEFT_TOP, LEFT_BOTTOM, RIGHT_TOP, RIGHT_BOTTOM, BOTTOM_LEFT, BOTTOM_RIGHT;
+    public JoystickButton TRIGGER, TOP_MIDDLE, TOP_BOTTOM, TOP_LEFT, TOP_RIGHT, LEFT_TOP, LEFT_BOTTOM, RIGHT_TOP, RIGHT_BOTTOM, BOTTOM_LEFT, BOTTOM_RIGHT;
 
     public AttackJoystickButtons(Joystick joystick) {
         TRIGGER = new JoystickButton(joystick, AttackJoystickButtonBindings.TRIGGER);
         TOP_MIDDLE = new JoystickButton(joystick, AttackJoystickButtonBindings.TOP_MIDDLE);
         TOP_LEFT = new JoystickButton(joystick, AttackJoystickButtonBindings.TOP_LEFT);
         TOP_RIGHT = new JoystickButton(joystick, AttackJoystickButtonBindings.TOP_RIGHT);
+        TOP_BOTTOM = new JoystickButton(joystick, AttackJoystickButtonBindings.TOP_BOTTOM);
         LEFT_TOP = new JoystickButton(joystick, AttackJoystickButtonBindings.LEFT_TOP);
         LEFT_BOTTOM = new JoystickButton(joystick, AttackJoystickButtonBindings.LEFT_BOTTOM);
         RIGHT_TOP = new JoystickButton(joystick, AttackJoystickButtonBindings.RIGHT_TOP);
