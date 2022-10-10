5b2f096564bc930f87b89e8023ff50e0441916f2
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-10-10 18:48:44 -0400

    updated joystick ids

--- src/main/java/org/talon540/control/AttackJoystick/AttackJoystickButtonBindings.java
+++ src/main/java/org/talon540/control/AttackJoystick/AttackJoystickButtonBindings.java
@@ -1,15 +1,15 @@
 package org.talon540.control.AttackJoystick;
 
 public interface AttackJoystickButtonBindings {
-    public int TRIGGER = 0; // TODO
-    public int TOP_MIDDLE = 0; // TODO
-    public int TOP_BOTTOM = 0; // TODO
-    public int TOP_LEFT = 0; // TODO
-    public int TOP_RIGHT = 0; // TODO
-    public int LEFT_TOP = 0; // TODO
-    public int LEFT_BOTTOM = 0; // TODO
-    public int RIGHT_TOP = 0; // TODO
-    public int RIGHT_BOTTOM = 0; // TODO
-    public int BOTTOM_LEFT = 0; // TODO
-    public int BOTTOM_RIGHT = 0; // TODO
+    public int TRIGGER = 1; // TODO
+    public int TOP_MIDDLE = 3; // TODO
+    public int TOP_BOTTOM = 2; // TODO
+    public int TOP_LEFT = 4; // TODO
+    public int TOP_RIGHT = 5; // TODO
+    public int LEFT_TOP = 6; // TODO
+    public int LEFT_BOTTOM = 7; // TODO
+    public int RIGHT_TOP = 11; // TODO
+    public int RIGHT_BOTTOM = 10; // TODO
+    public int BOTTOM_LEFT = 8; // TODO
+    public int BOTTOM_RIGHT = 9; // TODO
 }
