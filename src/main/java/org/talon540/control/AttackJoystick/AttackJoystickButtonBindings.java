9c5694d3591072ce54a6d3b9707ddb57f535e14d
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-10-18 23:05:54 -0400

    removed TODOs for key bind mappings, closes #4

--- src/main/java/org/talon540/control/AttackJoystick/AttackJoystickButtonBindings.java
+++ src/main/java/org/talon540/control/AttackJoystick/AttackJoystickButtonBindings.java
@@ -1,15 +1,15 @@
 package org.talon540.control.AttackJoystick;
 
 public interface AttackJoystickButtonBindings {
-    public int TRIGGER = 1; // TODO
-    public int TOP_MIDDLE = 3; // TODO
-    public int TOP_BOTTOM = 2; // TODO
-    public int TOP_LEFT = 4; // TODO
-    public int TOP_RIGHT = 5; // TODO
-    public int LEFT_TOP = 6; // TODO
-    public int LEFT_BOTTOM = 7; // TODO
-    public int RIGHT_TOP = 11; // TODO
-    public int RIGHT_BOTTOM = 10; // TODO
-    public int BOTTOM_LEFT = 8; // TODO
-    public int BOTTOM_RIGHT = 9; // TODO
+    public int TRIGGER = 1;
+    public int TOP_MIDDLE = 3;
+    public int TOP_BOTTOM = 2;
+    public int TOP_LEFT = 4;
+    public int TOP_RIGHT = 5;
+    public int LEFT_TOP = 6;
+    public int LEFT_BOTTOM = 7;
+    public int RIGHT_TOP = 11;
+    public int RIGHT_BOTTOM = 10;
+    public int BOTTOM_LEFT = 8;
+    public int BOTTOM_RIGHT = 9;
 }
