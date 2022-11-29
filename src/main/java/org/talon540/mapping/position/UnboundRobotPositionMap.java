81ffe153c07b2d14f4f9413b993964016201f0e7
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-11-28 20:45:48 -0500

    added null checking for position map

--- src/main/java/org/talon540/mapping/position/UnboundRobotPositionMap.java
+++ src/main/java/org/talon540/mapping/position/UnboundRobotPositionMap.java
@@ -8,8 +8,8 @@ import java.util.TreeMap;
 
 
 /**
- * An object used in the logging and retrieving of the Robot's position on the
- * field in the form of a {@link Pose2d} object
+ * An object used in the logging and retrieving of the Robot's position on the field in the form of a {@link Pose2d}
+ * object
  */
 public class UnboundRobotPositionMap implements Sendable {
     protected final TreeMap<Double, Pose2d> map = new TreeMap<>();
@@ -17,35 +17,35 @@ public class UnboundRobotPositionMap implements Sendable {
     /**
      * Add a position to the position map
      *
-     * @param position position of the robot in the form of a Pose2d object
+     * @param position position of the robot in the form of a {@link Pose2d} object
      * @param timestamp timestamp key
      */
     public void addPositionToMap(Pose2d position, double timestamp) {
-        this.map.put(timestamp, position);
+        this.map.put(
+                timestamp,
+                position
+        );
     }
 
     /**
-     * Get the last entered position in the position map
+     * Get the last entered position in the position map. Returns null if the map is empty
      */
     public Pose2d getLatestPosition() {
-        // return this.map.lastEntry().getValue();
-
-        try {
-            return this.map.lastEntry().getValue();
-        } catch (Exception e) {
-            return new Pose2d();
-        }
+        return this.map.isEmpty() ? null : this.map.lastEntry().getValue();
     }
 
     /**
-     * Get the estimated position of the robot at a specific time. If the time
-     * provided is between two different positions, it will interpolate the
-     * difference between the two timeslots and return the estimated position
+     * Get the estimated position of the robot at a specific time. If the time provided is between two different
+     * positions, it will interpolate the difference between the two timeslots and return the estimated position.
+     * Returns null if the map is empty
      *
      * @param timestamp timestamp to reference
      * @return robot's position on the field
      */
     public Pose2d getPositionFromTimestamp(double timestamp) {
+        if (this.map.isEmpty())
+            return null;
+
         if (timestamp <= this.map.firstKey()) {
             return this.map.firstEntry().getValue();
         }
@@ -57,19 +57,28 @@ public class UnboundRobotPositionMap implements Sendable {
         double lowerKeyBound = this.map.floorKey(timestamp);
         double ceilingKeyBound = this.map.ceilingKey(timestamp);
 
-        return this.map.get(lowerKeyBound).interpolate(this.map.get(ceilingKeyBound), (timestamp - lowerKeyBound) / ((ceilingKeyBound - lowerKeyBound)));
+        return this.map.get(lowerKeyBound).interpolate(
+                this.map.get(ceilingKeyBound),
+                (timestamp - lowerKeyBound) / ((ceilingKeyBound - lowerKeyBound))
+        );
     }
 
     @Override
     public void initSendable(SendableBuilder builder) {
-        builder.addDoubleProperty("count", map::size, null);
-        builder.addStringProperty("cPosition", () -> getLatestPosition().toString(), null);
-        builder.addDoubleProperty("cTimestamp", () -> {
-            try {
-                return map.firstKey();
-            } catch (Exception e) {
-                return 0;
-            }
-        }, null);
+        builder.addDoubleProperty(
+                "count",
+                map::size,
+                null
+        );
+        builder.addStringProperty(
+                "cPosition",
+                () -> this.map.isEmpty() ? "Empty Map" : getLatestPosition().toString(),
+                null
+        );
+        builder.addDoubleProperty(
+                "cTimestamp",
+                () -> this.map.isEmpty() ? 0 : map.firstKey(),
+                null
+        );
     }
 }
