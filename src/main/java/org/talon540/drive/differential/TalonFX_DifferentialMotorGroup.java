635a693b54bf4c02382e0053aca73324c3a044c2
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-12-07 22:45:12 -0500

    Update TalonFX_DifferentialMotorGroup.java

--- src/main/java/org/talon540/drive/differential/TalonFX_DifferentialMotorGroup.java
+++ src/main/java/org/talon540/drive/differential/TalonFX_DifferentialMotorGroup.java
@@ -9,24 +9,31 @@ public class TalonFX_DifferentialMotorGroup extends MotorControllerGroup {
     private final TalonFXSensorCollection sensorGroup;
 
     public TalonFX_DifferentialMotorGroup(WPI_TalonFX leader, WPI_TalonFX follower) {
-        super(leader, follower);
+        super(
+                leader,
+                follower
+        );
 
         sensorGroup = leader.getSensorCollection();
     }
 
+    public TalonFXSensorCollection getSensorGroup() {
+        return sensorGroup;
+    }
+
     /**
-     * Return the velocity of the motor group
-     * Return the TalonFX integrated sensor velocity in {@code CTRE ticks per 100ms}
+     * Return the velocity of the motor group Return the TalonFX integrated sensor velocity in
+     * {@code CTRE ticks per 100ms}
      */
-    public double getVelocity() {
+    public double getRawVelocity() {
         return sensorGroup.getIntegratedSensorVelocity();
     }
 
     /**
-     * Return the position of the motor group
-     * Return the TalonFX integrated sensor velocity in {@code CTRE ticks per 100ms}
+     * Return the position of the motor group Return the TalonFX integrated sensor velocity in
+     * {@code CTRE ticks per 100ms}
      */
-    public double getPosition() {
+    public double getRawPosition() {
         return sensorGroup.getIntegratedSensorPosition();
     }
 }
