bbee267891b2cf5181bf63c9df9bb0f80ab5381e
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-12-12 20:35:53 -0500

    Renamed BatteryVoltageCheck

---
+++ src/main/java/org/talon540/control/BatteryVoltageTrigger.java
@@ -0,0 +1,19 @@
+package org.talon540.control;
+
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+public class BatteryVoltageTrigger extends Trigger {
+    /**
+     * Set the minimum voltage required before the battery safety reporting process starts
+     *
+     * @param minVoltage minimum voltage [0v, 12v]
+     */
+    public BatteryVoltageTrigger(double minVoltage) {
+        super(() -> RobotController.getBatteryVoltage() < minVoltage);
+    }
+
+    public BatteryVoltageTrigger() {
+        this(11);
+    }
+}
