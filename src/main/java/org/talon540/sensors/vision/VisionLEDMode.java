03c1327b4bb338cbddeb83626907d9564440d5f1
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-12-30 02:58:54 -0500

    Formatting fixes

--- src/main/java/org/talon540/sensors/vision/VisionLEDMode.java
+++ src/main/java/org/talon540/sensors/vision/VisionLEDMode.java
@@ -1,20 +1,12 @@
 package org.talon540.sensors.vision;
 
 public enum VisionLEDMode {
-    /**
-     * Turn on the processioning unit's LEDs
-     */
-    kOn,
-    /**
-     * Turn off the processioning unit's LEDs
-     */
-    kOff,
-    /**
-     * Blink the processioning unit's LEDs
-     */
-    kBlink,
-    /**
-     * Set the processioning unit's LEDs to the pipeline or camera default
-     */
-    kDefault
+  /** Turn on the processioning unit's LEDs */
+  kOn,
+  /** Turn off the processioning unit's LEDs */
+  kOff,
+  /** Blink the processioning unit's LEDs */
+  kBlink,
+  /** Set the processioning unit's LEDs to the pipeline or camera default */
+  kDefault
 }
