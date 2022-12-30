5a0ee57a83f67014eff517a74b2cecb8eb458e39
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-12-30 01:41:39 -0500

    update docs and add origin accessor

--- src/main/java/org/talon540/sensors/vision/VisionCameraMountConfig.java
+++ src/main/java/org/talon540/sensors/vision/VisionCameraMountConfig.java
@@ -5,70 +5,78 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import java.util.Objects;
 import java.util.function.Supplier;
-import org.jetbrains.annotations.NotNull;
 
 /**
- * Represents the position of a vision camera relative to the origin of the robot in the RCS or <a
+ * Represents the position of a vision camera relative to the origin of the robot in the <a
  * href="https://docs.wpilib.org/en/stable/docs/software/advanced-controls/geometry/coordinate-systems.html#robot-coordinate-system">Robot
- * Coordinate System</a>. For most use cases, an origin point of {@code new Pose3d()} is the
- * default.
+ * Coordinate System</a>.
  */
 public class VisionCameraMountConfig {
-  private final Pose3d robotOrigin;
+  private final Pose3d robotOriginPosition;
   private final Supplier<Pose3d> cameraPosition;
 
   /**
-   * Construct a CameraMountConfig from a {@link Pose3d} supplier. This can be useful if the camera
-   * is mounted on a non-static surface such as a turret or telescoping pole. The supplier
+   * Construct a VisionCameraMountConfig from a {@link Pose3d} supplier. This can be useful if the
+   * camera is mounted on a non-static surface such as a turret or telescoping pole. The supplier
    * <b>cannot</b> return null or else an error will be thrown.
    *
-   * @param robotOrigin {@link Pose3d} representing the position of the robot origin in the RCS.
+   * @param robotOriginPosition {@link Pose3d} representing the position of the robot origin in the
+   *     RCS.
    * @param cameraPosition {@link Pose3d} supplier that returns the position of the camera in the
    *     RCS.
    */
-  public VisionCameraMountConfig(
-      @NotNull Pose3d robotOrigin, @NotNull Supplier<Pose3d> cameraPosition) {
-    if (!robotOrigin.getRotation().equals(new Rotation3d())) {
-      throw new IllegalArgumentException(
-          "The robot origin cannot have different yaw, pitch, or skew. It must hold a Rotation3d with a standard values.");
-    }
+  public VisionCameraMountConfig(Pose3d robotOriginPosition, Supplier<Pose3d> cameraPosition) {
+    this.robotOriginPosition = Objects.requireNonNull(robotOriginPosition);
+    this.cameraPosition = Objects.requireNonNull(cameraPosition);
 
-    this.robotOrigin = robotOrigin;
-    this.cameraPosition = cameraPosition;
+    if (!robotOriginPosition.getRotation().equals(new Rotation3d())) {
+      throw new IllegalArgumentException(
+          "Robot origin cannot have a non-zero roll, pitch, or yaw.");
+    }
   }
 
   /**
-   * Construct a CameraMountConfig from a {@link Pose3d} supplier. This can be useful if the camera
-   * is mounted on a non-static surface such as a turret or telescoping pole. The supplier
+   * Construct a VisionCameraMountConfig from a {@link Pose3d} supplier. This can be useful if the
+   * camera is mounted on a non-static surface such as a turret or telescoping pole. The supplier
    * <b>cannot</b> return null or else an error will be thrown. Assumes that the robot origin is the
    * point on the floor horizontally and vertically centered to the robot.
    *
    * @param cameraPosition {@link Pose3d} representing the position of the camera in the RCS.
    */
-  public VisionCameraMountConfig(@NotNull Supplier<Pose3d> cameraPosition) {
+  public VisionCameraMountConfig(Supplier<Pose3d> cameraPosition) {
     this(new Pose3d(), cameraPosition);
   }
 
   /**
-   * Construct a CameraMountConfig for a statically mounted camera from the position of the camera
-   * as a {@link Pose3d}.
+   * Construct a VisionCameraMountConfig for a statically mounted camera from the position of the
+   * camera as a {@link Pose3d}.
    *
-   * @param robotOrigin {@link Pose3d} representing the position of the robot origin in the RCS.
+   * @param robotOriginPosition {@link Pose3d} representing the position of the robot origin in the
+   *     RCS.
    * @param cameraPosition {@link Pose3d} representing the position of the camera in the RCS.
    */
-  public VisionCameraMountConfig(@NotNull Pose3d robotOrigin, @NotNull Pose3d cameraPosition) {
-    this(robotOrigin, () -> cameraPosition);
+  public VisionCameraMountConfig(Pose3d robotOriginPosition, Pose3d cameraPosition) {
+    this(robotOriginPosition, () -> cameraPosition);
   }
 
   /**
-   * Construct a CameraMountConfig for a statically mounted camera from the position of the camera
-   * as a {@link Pose3d}. Assumes that the robot origin is the point on the floor horizontally and
-   * vertically centered to the robot.
+   * Construct a VisionCameraMountConfig for a statically mounted camera from the position of the
+   * camera as a {@link Pose3d}. Assumes that the robot origin is the point on the floor
+   * horizontally and vertically centered to the robot.
    *
    * @param cameraPosition {@link Pose3d} of the position of the camera in the RCS.
    */
-  public VisionCameraMountConfig(@NotNull Pose3d cameraPosition) {
+  public VisionCameraMountConfig(Pose3d cameraPosition) {
     this(new Pose3d(), cameraPosition);
+  }
+
+  /**
+   * Return the specified robot origin in the RCS.
+   *
+   * @return robot origin in the RCS.
+   */
+  public Pose3d getRobotOriginPosition() {
+    return this.robotOriginPosition;
   }
 
   /**
@@ -81,17 +89,17 @@ public class VisionCameraMountConfig {
   }
 
   /**
-   * Return the current transform between the origin (center) of the robot to the camera.
+   * Return the current transform between the origin of the robot to the camera.
    *
    * @return current {@link Transform3d}.
    */
   public Transform3d getRobotToCamera() {
-    return new Transform3d(robotOrigin, cameraPosition.get());
+    return new Transform3d(robotOriginPosition, getCameraPosition());
   }
 
   /**
    * Return the pitch of the camera relative to the horizontal plane the camera sits on assuming the
-   * plane bisects the camera's sensor.
+   * plane bisects the camera's sensor. This angle is signed.
    *
    * @return camera pitch in radians.
    * @see <a
@@ -104,7 +112,7 @@ public class VisionCameraMountConfig {
 
   /**
    * Return the height of the camera relative to the origin of the robot. Assuming the origin has a
-   * height/Z of 0, this would be the height of the camera off the floor.
+   * height/Z value of 0, this would be the height of the camera off the floor.
    *
    * @return height of the camera relative to the origin of the robot.
    * @see <a
